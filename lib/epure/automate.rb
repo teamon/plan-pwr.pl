b04559456e59a9e3316ce9c69bc44f7e2e2dbab7
Tymon Tobolski <i@teamon.eu>
2011-09-14 21:35:22 +0200

    Fixed automated logging

--- lib/epure/automate.rb
+++ lib/epure/automate.rb
@@ -3,23 +3,31 @@
 module Epure
   class LoginException < Exception
   end
-  
+
   class Automate
     def self.run!(login, password)
       agent = Mechanize.new
-    
+
       form = agent.get("https://edukacja.pwr.wroc.pl/EdukacjaWeb/studia.do").forms.first
+
       form.login = login
       form.password = password
       page = form.submit
-    
+
       if page.title =~ /Błąd logowania/
         raise LoginException
       end
-    
-      agent.get "https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do?event=WyborSluchacza"
-      page = agent.post "https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do?href=#hrefZapisySzczSlu", "event_WyborZapisowWidok" => "Przełącz na <Grupy zajęciowe, do których słuchacz jest zapisany w semestrze>"
-    
+
+      page = page.link_with(:text => 'Zapisy').click
+
+      form = page.forms.find do |e|
+        e.buttons.any? do |f|
+          f.value == "Przełącz na <Grupy zajęciowe, do których słuchacz jest zapisany w semestrze>"
+        end
+      end
+
+      page = agent.submit(form, form.buttons.first)
+
       Epure::Parser.parse!(page.body)
     end
   end
