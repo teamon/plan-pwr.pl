0e45f75a505caa894a5e40519cd29fae79ce7cf5
Tymon Tobolski <i@teamon.eu>
2011-09-23 19:53:35 +0200

    Multiple date time support

--- lib/epure/parser.rb
+++ lib/epure/parser.rb
@@ -10,7 +10,7 @@ module Epure
 
   class Parser
     include Epure::Config
-    
+
     def self.parse!(html)
       doc = new(html)
       doc.parse!
@@ -21,91 +21,86 @@ module Epure
       @doc = Nokogiri::HTML(html)
     end
 
+    def entry_time(e)
+      [e.week_day, e.week, e.start_hour, e.start_min, e.end_hour, e.end_min]
+    end
+
     def parse!
       schedule = Schedule.new
 
       semester = @doc.css("td.WYBRANA").map {|e| e.content.strip}
-      Rails.logger.debug("semester: " + semester.inspect)
-      
       schedule.year = semester[0]
-      Rails.logger.debug("s.year: " + semester[0])
-      
       schedule.semester = SEMESTERS[semester[1]]
-      Rails.logger.debug("s.semester: " + semester[1])
-      
-      tables =  @doc.css("table.KOLOROWA")
-    
+
+      tables = @doc.css("table.KOLOROWA")
       entries = []
-      
+
       tables[2, tables.size-2].each do |table|
-        Rails.logger.debug("---")
         trs = table.children
         trs = trs[4, trs.size-4]
-    
+
         (0...(trs.size / 4)).each do |i|
-          Rails.logger.debug("---")
           k = 4*i
           entry = Entry.new
-    
+
           tds = trs[k].css("td")
-          
+
           if tds.size > 2
-          
-            Rails.logger.debug("tds.size: #{tds.size}")
-            # Rails.logger.debug("tds: " + tds.inspect)
             entry.group_code = tds[0].content.strip
-            Rails.logger.debug("entry.group_code: #{entry.group_code}")
-          
             entry.course_code = tds[1].content.strip
-            Rails.logger.debug("entry.course_code: #{entry.course_code}")
-          
             entry.course_name = tds[2].content.strip
-            Rails.logger.debug("entry.course_name: #{entry.course_name}")
-    
+
             tds = trs[k+2].css("td")
             entry.lecturer = tds[0].content.strip
             type = tds[1].content.strip
             entry.course_type = COURSE_TYPES[type] || type
-        
-            trs[k+3].css("table tr td").each do |td|
+
+            es = trs[k+3].css("table tr td").map do |td|
+              e = entry.dup
+
               where_when = td.content.strip
-            
-              Rails.logger.debug("where_when: " + where_when)
-            
               m_when = nil
-          
+
               if m_when = where_when.match(/\d{4}-\d{2}-\d{2} \((.+?)\) (\d{2}):(\d{2})-(\d{2}):(\d{2})/u)
                 # zaoczne
-                Rails.logger.debug("Matched where_when #2")
-                entry.week_day    = WEEK_DAYS[m_when[1]]
-                entry.week        = WEEKS[""]
-                entry.start_hour  = m_when[2].to_i
-                entry.start_min   = m_when[3].to_i
-                entry.end_hour    = m_when[4].to_i
-                entry.end_min     = m_when[5].to_i
+                e.week_day    = WEEK_DAYS[m_when[1]]
+                e.week        = WEEKS[""]
+                e.start_hour  = m_when[2].to_i
+                e.start_min   = m_when[3].to_i
+                e.end_hour    = m_when[4].to_i
+                e.end_min     = m_when[5].to_i
               elsif m_when = where_when.match(/(.{2})(?:\/(T(?:P|N)?))? (\d{2}):(\d{2})-(\d{2}):(\d{2})/u)
-                Rails.logger.debug("Matched where_when #1")
-                entry.week_day    = WEEK_DAYS[m_when[1]]
-                entry.week        = WEEKS[m_when[2] || ""]
-                entry.start_hour  = m_when[3].to_i
-                entry.start_min   = m_when[4].to_i
-                entry.end_hour    = m_when[5].to_i
-                entry.end_min     = m_when[6].to_i
+                e.week_day    = WEEK_DAYS[m_when[1]]
+                e.week        = WEEKS[m_when[2] || ""]
+                e.start_hour  = m_when[3].to_i
+                e.start_min   = m_when[4].to_i
+                e.end_hour    = m_when[5].to_i
+                e.end_min     = m_when[6].to_i
               end
-            
+
               unless m_when.nil?
                 if m_where = where_when.match(/bud. (.+?), sala (.+)/u)
-                  entry.building = m_where[1]
-                  entry.room = m_where[2]
+                  e.building = m_where[1]
+                  e.room = m_where[2]
                 else
-                  entry.building = "?"
-                  entry.room = "?"
+                  e.building = "?"
+                  e.room = "?"
                 end
-              
-                entries << entry
+                e
+              else
+                nil
               end
+
+            end.compact
+
+            es.reject do |e|
+              et = entry_time(e)
+              e.building == "?" && e.room == "?" && es.find {|x| entry_time(x) == et }
+            end.uniq do |e|
+              entry_time(e)
+            end.each do |e|
+              entries << e
             end
-            
           end
 
         end
