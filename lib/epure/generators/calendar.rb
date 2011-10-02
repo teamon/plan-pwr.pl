27c6c887d315a0ca15ec7e4b1cc3518cc3023161
Tymon Tobolski <i@teamon.eu>
2011-10-02 22:14:30 +0200

    Fix ics/vcs export for sunday

--- lib/epure/generators/calendar.rb
+++ lib/epure/generators/calendar.rb
@@ -7,7 +7,7 @@ module Epure
     class Calendar < Generator
       def each
         Epure::EDate.dates_for(@schedule).each do |date|
-          if entries = @schedule.days[date.wday-1]
+          if entries = @schedule.days[(date.wday-1) % 7]
             entries.select {|e| e.in_week?(date.cweek) }.each do |entry|
               yield(date, entry)
             end
