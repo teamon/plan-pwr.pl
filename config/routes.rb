b76879abf4ad175fc7146dbf4856a2791136d01e
Tymon Tobolski <i@teamon.eu>
2012-01-31 22:55:06 +0100

    apel

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Epure::Application.routes.draw do
         get :search_lecturers
       end
     end
-    
+
     get "color_schemes", :to => "color_schemes#index"
     post "color_schemes/reset", :to => "color_schemes#reset"
     post "color_schemes/bw", :to => "color_schemes#bw"
   end
-  
+
+  get "/apel", :to => "application#apel"
   get "/:slug", :to => "schedules#show", :as => :schedule_slug
   root :to => "schedules#index"
 end
