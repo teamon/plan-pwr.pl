fd932fb8c7d618a962e55e1d6e3cea255e008794
hodak <kaczor555@gmail.com>
2014-02-25 00:41:07 +0100

    New or updated tests

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -4,12 +4,12 @@ describe Schedule do
   before(:each) do
     @schedule = Schedule.create
   end
-  
+
   describe "color schemes" do
     it "should create default color schemes" do
       @schedule.color_schemes.size.should == 6
     end
-    
+
     it "should set new color scheme" do
       @schedule.set_color_scheme(:bw)
       @schedule.color_schemes.size.should == 6
@@ -25,11 +25,11 @@ describe Schedule do
   it "should create slug" do
     @schedule.slug.should_not be_blank
   end
-  
+
   it "should return correct semester name" do
     @schedule.semester = 1
     @schedule.semester_name.should == "Zimowy"
-    
+
     @schedule.semester = 2
     @schedule.semester_name.should == "Letni"
   end
